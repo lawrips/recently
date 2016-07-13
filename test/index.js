16d65e1054b80eecb592a3f9853fb9630bb136f0
Lawrence Ripsher <lawrips@microsoft.com>
2016-07-12 22:44:40 -0700

    updated tests

--- test/index.js
+++ test/index.js
@@ -6,8 +6,14 @@ let Recently = require('../lib/index');
 
 
 describe('tests', () => {
+    it('constructor', (done) => {
+        let cache = new Recently();
+        done();
+    });
+
     it('last(num)', (done) => {
         let cache = new Recently();
+        JSON.stringify(cache.last(1)).should.equal('[]');
         cache.add(1);
         cache.add(2);
         cache.add(3);
@@ -15,11 +21,17 @@ describe('tests', () => {
         JSON.stringify(cache.last(3)).should.equal('[1,2,3]');
         JSON.stringify(cache.last(2)).should.equal('[2,3]');
         JSON.stringify(cache.last(1)).should.equal('[3]');
+
+        cache.clear();
+        let obj = [{'hello':'world'}];
+        cache = new Recently(obj);
+        JSON.stringify(cache.last(1)).should.equal(JSON.stringify(obj));
         done();
     });
 
     it('last(value, nun)', (done) => {
         let cache = new Recently();
+        cache.last(true, 1).should.equal(0);
         cache.add(true);
         cache.add(true);
         cache.add(false);
@@ -31,6 +43,23 @@ describe('tests', () => {
         cache.last(false, 5).should.equal(3);
         cache.last(false, 2).should.equal(2);
         cache.last(false, 1).should.equal(1);
+
+        cache.clear();
+        let obj = [{'hello':'world'}];
+        cache = new Recently(obj);
+        cache.last(obj[0],1).should.equal(1);
+
+        cache.clear();
+        let deepObj = [{'hello':{'hello': 'world'}}];
+        cache = new Recently(deepObj);
+        cache.last(deepObj[0],1).should.equal(1);
+
+        cache.clear();
+        let array = ['a','b','c'];
+        cache = new Recently();
+        cache.add(array)
+        cache.last(array,1).should.equal(1);
+
         done();
     });
 
@@ -43,20 +72,35 @@ describe('tests', () => {
         done();
     });
 
+    it('clear()', (done) => {
+        let cache = new Recently([1,2,3]);
+        JSON.stringify(cache.last(4)).should.equal('[1,2,3]');
+        cache.clear();
+        JSON.stringify(cache.last(4)).should.equal('[]');
+        cache = new Recently([1,2,3]);
+        JSON.stringify(cache.last(4)).should.equal('[1,2,3]');
+        done();
+    });
+
     it('ago(seconds)', (done) => {
         let cache = new Recently();
+        JSON.stringify(cache.ago(1)).should.be.equal('[]');
         cache.add(true);
         setTimeout(() => {
             cache.add(true);
 
             setTimeout(() => {
-
                 JSON.stringify(cache.ago(1)).should.be.equal('[]');
                 cache.add(false);
                 JSON.stringify(cache.ago(1)).should.be.equal('[false]');
                 JSON.stringify(cache.ago(2)).should.be.equal('[true,false]');
                 JSON.stringify(cache.ago(3)).should.be.equal('[true,true,false]');
                 JSON.stringify(cache.ago(4)).should.be.equal('[true,true,false]');
+
+                cache.clear();
+                let obj = [{'hello':'world'}];
+                cache = new Recently(obj);
+                JSON.stringify(cache.ago(1)).should.equal(JSON.stringify(obj));
                 done();
                         
             }, 1500);
@@ -77,6 +121,23 @@ describe('tests', () => {
                 cache.ago(true, 4).should.be.equal(2);
                 cache.ago(false, 2).should.be.equal(1);
                 cache.ago(false, 10).should.be.equal(1);
+
+
+                cache.clear();
+                let obj = [{'hello':'world'}];
+                cache = new Recently(obj);
+                cache.ago(obj[0],1).should.equal(1);
+
+                cache.clear();
+                let deepObj = [{'hello':{'hello': 'world'}}];
+                cache = new Recently(deepObj);
+                cache.ago(deepObj[0],1).should.equal(1);
+
+                cache.clear();
+                let array = ['a','b','c'];
+                cache = new Recently();
+                cache.add(array)
+                cache.ago(array,1).should.equal(1);
 
                 done();
                         
